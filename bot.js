d69951d1a7af88ef58cb55580f972d7eabab1fff
William Cottam <38957412+YCougFan@users.noreply.github.com>
2018-11-14 11:20:23 -0700

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log("Connected as " + client.user.tag)
+
+    client.user.setActivity("YouTube", {type: "WATCHING"})
+
+    client.guilds.forEach((guild) => {
+        console.log(guild.name)
+        guild.channels.forEach((channel) => {
+            console.log(` - ${channel.name} ${channel.type} ${channel.id}`)
+        })
+        //General Channel ID 512298809779421206
+    })
+    let generalChannel = client.channels.get("512298809779421206")
+    const attachment = new Discord.Attachment("https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fpearlsofprofundity.files.wordpress.com%2F2013%2F02%2Ffamily-tree-with-apple.jpg&f=1")
+    generalChannel.send(attachment)
+})
+
+//Bot Token
+client.login("NTEyMzAwMjk2MTM1NzcwMTMx.Ds3fog.rbxXFlSTss39cYn712CcS27sJhc")
